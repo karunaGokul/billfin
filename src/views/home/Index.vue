cb59d3317a51255ffdd06704cc2005564e151135
LogeswaranS <logeswaran@yectra.com>
2021-11-29 13:38:18 +0530

    Bugs fixed

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -505,7 +505,6 @@ export default class Home extends Vue {
       if (this.firms[0].trialStartsOn && this.firms[0].trialEndsOn)
         this.trailExpireDays();
       this.store.dispatch("loadEntitlements", response[0]);
-      this.showOnBoard = true;
       if (
         this.firms.length == 1 &&
         this.firms[0].trialOnboardingStatus != "COMPLETED"
